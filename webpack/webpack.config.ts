cd39e28c513f252733675f84a8bb949f6946de56
sagar.arora <sagar.arora@surakshitnivesh.com>
2024-12-12 18:59:00 +0530

    latest

--- webpack/webpack.config.ts
+++ webpack/webpack.config.ts
@@ -1,6 +1,53 @@
 import path from 'path';
+// import HtmlWebpackPlugin from 'html-webpack-plugin';
+// import { plugins } from './webpack.client';
+
+
+import { WebpackPluginInstance } from 'webpack';
 import HtmlWebpackPlugin from 'html-webpack-plugin';
-import { plugins } from './webpack.client';
+import ForkTsCheckerWebpackPlugin from 'fork-ts-checker-webpack-plugin';
+import MiniCssExtractPlugin from 'mini-css-extract-plugin';
+import LoadablePlugin from '@loadable/webpack-plugin';
+import ReactRefreshWebpackPlugin from '@pmmmwh/react-refresh-webpack-plugin';
+
+
+
+import pkg from 'webpack';
+
+
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
+const { HotModuleReplacementPlugin, DefinePlugin } = pkg;
+
+const IS_DEV = false;
+const STATIC_CONTENT_PATH = 'pagination';
+
+const plugins: WebpackPluginInstance[] = [
+    new DefinePlugin({
+        NO_SSR: process.env.NO_SSR === 'true',
+    }),
+    ...(process.env.NO_SSR === 'true' ? [
+        new HtmlWebpackPlugin({
+            template: './src/client/index.html',
+          }),
+    ] : []),
+    new ForkTsCheckerWebpackPlugin(),
+    new MiniCssExtractPlugin({
+        filename: IS_DEV ? `${STATIC_CONTENT_PATH}css/[name].css` : `${STATIC_CONTENT_PATH}css/[name].[contenthash].css`,
+    }),
+    new LoadablePlugin(
+        {
+            filename: 'stats.json',
+            writeToDisk: true,
+        }) as { apply(): void; },
+    ...(IS_DEV ? [
+        new HotModuleReplacementPlugin(),
+        new ReactRefreshWebpackPlugin(),
+    ] : [
+    ]),
+];
+
 
 // const configs: any[] = []
 
@@ -35,7 +82,7 @@ const clientConfig = {
     filename: 'client.js',
     path: path.resolve(__dirname, '../dist/client'),
   },
-  plugins,
+  plugins:plugins,
   target: 'web', // For browser environment
 };
 
@@ -60,7 +107,7 @@ const serverConfig = {
 };
 
 
-const IS_DEV = false;
+
 
 if (process.env.NO_SSR === 'true') {
   configs.push(clientConfig);
@@ -71,5 +118,7 @@ if (process.env.NO_SSR === 'true') {
     configs.push(clientConfig);
   }
 }
+
+
 
 export default configs;
